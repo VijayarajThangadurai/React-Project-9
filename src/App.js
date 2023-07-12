962a8aabcd5e5d0145ed0be0132b1d5b9cb54749
VijayarajThangadurai <124797617+VijayarajThangadurai@users.noreply.github.com>
2023-07-12 20:12:47 +0530

    Building Blocks of Ecommerce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import { Button, Container} from "react-bootstrap";
+import React from 'react';
+import './App.css';
+import Products from "./Components/Designing/Products";
+import Navbar from 'react-bootstrap/Navbar';
+
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <Navbar bg="dark" variant="dark" expand ="sm">
+  <Container>
+  <Navbar.Brand href="#home">E-commerce</Navbar.Brand>
+   <Button variant="link">Home</Button>
+   <Button variant="link">Store</Button>
+   <Button variant="link">About</Button>
+   <Button>Cart</Button>
+   </Container>
+   </Navbar>
+   <Products/>
+   </>
+  
   );
-}
+};
 
 export default App;
+
